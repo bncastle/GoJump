617cc54b7020f333e4d75b4179bf452ffee04c48
Bryan Castleberry <bncastle@pixelbytestudios.com>
2019-04-18 10:47:11 -0400

    Episode 8

--- GoJump/Game.gd
+++ GoJump/Game.gd
@@ -5,7 +5,7 @@ const LVL_PATH = "res://Levels/Level%d.tscn"
 var lvl_num:int = 1
 
 func _ready():
-	add_to_group("pickup_listeners")
+	add_to_group("game")
 	call_deferred("init")
 
 func init():
@@ -14,12 +14,22 @@ func init():
 func load_level(num:int):
 	var root = get_tree().root
 	if root.has_node("Level"):
-		root.remove_child($Level)
+		var lvl = root.get_node("Level")
+		root.remove_child(lvl)
+		lvl.queue_free()
 
 	#todo:check if level actually exists
 	var lvl = load(LVL_PATH % num).instance()
 	root.add_child(lvl)
 	return true
+
+##Game group functions
+
+func on_next_level():
+	lvl_num += 1
+	#todo: we want a scen transition of some sort and maybe some "game juice" here
+	load_level(lvl_num)
+	#todo: something if no more levels
 
 func on_pickup(item):
 	if item.name == "Key":
--- GoJump/Pickups/BasePickup.gd
+++ GoJump/Pickups/BasePickup.gd
@@ -4,5 +4,5 @@ func _ready():
 	connect("body_entered", self, "on_player_entered")
 
 func on_player_entered(body):
-	get_tree().call_group("pickup_listeners","on_pickup", self)
+	get_tree().call_group("game","on_pickup", self)
 	queue_free()
