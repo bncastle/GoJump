0ee75bfff52744cfc379f700bc2becd6f9c7ef1d
bncastle <bncastle@pixelbytestudios.com>
2021-09-08 13:57:50 -0400

    Episode 6

--- GoJump/Levels/Level.gd
+++ GoJump/Levels/Level.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
+const KEY := 2
+const COIN := 8
 const PLAYER := 13
 
 export (PackedScene) var player
+export (PackedScene) var coin
+export (PackedScene) var key
 
 func _ready():
 	call_deferred("setup_tiles")
@@ -12,8 +16,13 @@ func setup_tiles():
 	for cell in cells:
 		var index = $Tiles.get_cell(cell.x, cell.y)
 		match index:
+			KEY:
+				create_instance_from_tilemap(cell, key, $Items, Vector2(6,6))
+			COIN:
+				create_instance_from_tilemap(cell, coin, $Items, Vector2(6,6))
 			PLAYER:
 				create_instance_from_tilemap(cell, player, self, Vector2(6,12))
+
 
 func create_instance_from_tilemap(coord:Vector2, prefab:PackedScene, parent: Node2D, offset:Vector2 = Vector2.ZERO):
 	$Tiles.set_cell(coord.x, coord.y, -1)
