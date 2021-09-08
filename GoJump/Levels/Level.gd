9b9441a940c11027da8cce1ec35ea302bd9e1889
bncastle <bncastle@pixelbytestudios.com>
2021-09-08 13:58:30 -0400

    Episode 14
    Episode 14 wherein I add computer triggering to replace blocks from the block outlines.  Additionally we improve the coins by adding random start positions to their animations.

--- GoJump/Levels/Level.gd
+++ GoJump/Levels/Level.gd
@@ -1,10 +1,13 @@
 extends Node2D
+class_name Level
 
 const KEY := 2
 const DOOR := 4
 const CHAIN := 5
+const BLOCK := 6
 const LADDER := 7
 const COIN := 8
+const BLOCK_OUTLINE := 9
 const PLAYER := 13
 const COMPUTER := 14
 const LADDER_TOP := 15
@@ -39,9 +42,11 @@ func setup_tiles():
 			PLAYER:
 				create_instance_from_tilemap(cell, player, self, Vector2(6,12))
 			COMPUTER:
-				create_instance_from_tilemap(cell, computer, self, Vector2(6,12))
+				create_instance_from_tilemap(cell, computer, self, Vector2(6,6))
 			LADDER_TOP:
 				create_instance_from_tilemap(cell, ladder_top, $Interactables, Vector2(6,6))
+			COMPUTER:
+				create_instance_from_tilemap(cell, computer, $Triggerables, Vector2(6,6))
 
 func create_instance_from_tilemap(coord:Vector2, prefab:PackedScene, parent: Node2D, offset:Vector2 = Vector2.ZERO):
 	$Tiles.set_cell(coord.x, coord.y, -1)
@@ -49,3 +54,10 @@ func create_instance_from_tilemap(coord:Vector2, prefab:PackedScene, parent: Nod
 	pf.position = $Tiles.map_to_world(coord) + offset
 	parent.add_child(pf)
 
+#func get_tiles(tile_index:int) -> Array:
+#	return $Tiles.get_used_cells_by_id(tile_index)
+
+func replace_tiles(old_tile_index:int, new_tile_index:int):
+	var cells = $Tiles.get_used_cells_by_id(old_tile_index)
+	for cell in cells:
+		$Tiles.set_cell(cell.x, cell.y, new_tile_index)
--- GoJump/Pickups/BasePickup.gd
+++ GoJump/Pickups/BasePickup.gd
@@ -1,7 +1,13 @@
 extends Area2D
 
+export(bool) var anim_random_start = false
+
 func _ready():
 	connect("body_entered", self, "on_player_entered")
+
+	if anim_random_start:
+		var anim_offset = $Anim.current_animation_length * randf()
+		$Anim.advance(anim_offset)
 
 func on_player_entered(body):
 	get_tree().call_group("game","on_pickup", self)
