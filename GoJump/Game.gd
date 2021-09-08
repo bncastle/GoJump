a6c7526d1b381a9cbb3d3ed6ab79fea51764584a
bncastle <bncastle@pixelbytestudios.com>
2021-09-08 13:58:35 -0400

    Ep15 - Added Scene Fade/Pause

--- GoJump/Game.gd
+++ GoJump/Game.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 const LVL_PATH = "res://Levels/Level%d.tscn"
 
+export(float) var fade_time = 0.5
+
 var lvl_num:int = 1
 onready var Level = preload("res://Levels/Level.gd")
 
@@ -14,28 +16,36 @@ func init():
 
 func load_level(num:int):
 	var old_lvl = get_level_node()
-	var root = get_tree().root
 	if old_lvl != null:
-		root.remove_child(old_lvl)
+		self.remove_child(old_lvl)
 		old_lvl.queue_free()
 
 	#todo:check if level actually exists
 	var lvl = load(LVL_PATH % num).instance()
-	root.add_child(lvl)
+	self.add_child(lvl)
 	return true
 
 func get_level_node():
-	var root = get_tree().root
-	if root.has_node("Level"):
-		return root.get_node("Level")
+	if self.has_node("Level"):
+		return self.get_node("Level")
 	return null
 
 ##Game group functions
 
 func on_next_level():
 	lvl_num += 1
-	#todo: we want a scen transition of some sort and maybe some "game juice" here
+	#todo: we want a scene transition of some sort and maybe some "game juice" here
+	get_tree().paused = true
+	var f = $Container/Fader
+	$Tween.interpolate_property(f, "modulate:a", f.modulate.a, 1, fade_time, Tween.TRANS_CUBIC, Tween.EASE_OUT)
+	$Tween.start()
+	yield($Tween, "tween_completed")
 	load_level(lvl_num)
+	$Tween.interpolate_property(f, "modulate:a", f.modulate.a, 0, fade_time, Tween.TRANS_CUBIC, Tween.EASE_IN)
+	$Tween.start()
+	yield($Tween, "tween_completed")
+	get_tree().paused = false
+
 	#todo: something if no more levels
 
 func on_pickup(item):
